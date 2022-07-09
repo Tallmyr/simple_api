ce3858f29b6c2a4cde8d6ba3945d7aa411c3ee8e
Simon Tallmyr <simon@tallmyr.se>
2022-07-09 17:16:33 +0000

    Document OpenAPI Fixes #4

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
-from starlite import Starlite
+from starlite import OpenAPIConfig, Starlite
 
 from app.controllers.maths import MathsController
 
-app = Starlite(route_handlers=[MathsController])
+app = Starlite(
+    route_handlers=[MathsController],
+    openapi_config=OpenAPIConfig(
+        create_examples=True, title="Boomi Sample API's", version="1.0.0"
+    ),
+)
