9d02d5817acc7ddf0183ce109ad75c56bbbe6453
Simon Tallmyr <simon@tallmyr.se>
2022-07-09 16:58:47 +0000

    added logic api's Fixes Odd/even #3

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from starlite import Starlite
 
 from app.controllers.maths import MathsController
+from app.controllers.logic import LogicController
 
-app = Starlite(route_handlers=[MathsController])
+app = Starlite(route_handlers=[MathsController, LogicController])
